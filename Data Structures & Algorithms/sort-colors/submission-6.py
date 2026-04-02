90e7627748047a8b2420aabf14024883734d1a5b
shilpachandrappa <67675965+shilpachandrappa@users.noreply.github.com>
2026-04-02 15:47:20 -0700

    Add: sort-colors - submission-6

---
+++ Data Structures & Algorithms/sort-colors/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        index = 0
+        left, right = 0, len(nums) - 1
+
+        def swap(i,j):
+            nums[i], nums[j] = nums[j], nums[i]
+
+        while index <= right :
+            if nums[index] == 0 :
+                swap(index, left)
+                left += 1
+                index += 1
+            elif nums[index] == 2:
+                swap(index, right)
+                right -=1
+            else :
+                index += 1
