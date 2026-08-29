ff5704352542a48ba9af9442087bc74e745b7883
shilpachandrappa <67675965+shilpachandrappa@users.noreply.github.com>
2026-08-28 23:20:51 -0700

    Add: sql-employees-with-missing-information - submission-0

---
+++ SQL/sql-employees-with-missing-information/submission-0.sql
@@ -0,0 +1,8 @@
+SELECT employee_id
+FROM employees
+WHERE employee_id NOT IN (SELECT employee_id FROM salaries)
+UNION
+SELECT employee_id
+FROM salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM employees)
+ORDER BY employee_id;
